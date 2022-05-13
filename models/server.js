b5d12dd7603bc7bb148f8f95ca9b9d875e9c5874
camilo londoño <cl.mon9566@gmail.com>
2022-05-13 17:41:52 -0500

    estructurando proyecto

---
+++ models/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const { sequelize } = require("./database/db");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+  }
+
+  async listen() {
+    try {
+      await sequelize.authenticate();
+      console.log("Connection has been established successfully.");
+    } catch (error) {
+      console.error("Unable to connect to the database:", error);
+    }
+    //sequelize
+    //  .authenticate()
+    //  .then(() => {
+    //    console.log("Connection has been established successfully.");
+    // })
+    //  .catch((error) => {
+    //   console.error("Unable to connect to the database:", error);
+    // });
+  }
+}
+
+module.exports = Server;
